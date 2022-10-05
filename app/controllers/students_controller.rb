08b1199b9d567ad7efc9614cfc1a0eda44f2fda4
Seth David Gyimah <esec26@gmail.com>
2022-10-05 17:41:36 +0000

    completed all but bonus

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,6 +3,27 @@ class StudentsController < ApplicationController
   def index
     students = Student.all
     render json: students
+
+    # matches = []
+
+    # if params[:name]  
+    #   Student.all.each do |item|
+    #     if item.first_name.downcase == params[:name].downcase || item.first_name.downcase == params[:name].downcase
+    #       matches << item
+    #     else
+    #       Student.all
+    #     end
+    #     matches
+    #     render json: matches
+    # end
+
+
+  end
+
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
